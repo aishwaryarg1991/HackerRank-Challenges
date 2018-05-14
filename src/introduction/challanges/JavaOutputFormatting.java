269e9a10b654bb01fa89701eac6621037ce527e4
aishwaryarg1991 <30918908+aishwaryarg1991@users.noreply.github.com>
2018-05-14 00:31:25 -0400

    Comments commit.

--- src/introduction/challanges/JavaOutputFormatting.java
+++ src/introduction/challanges/JavaOutputFormatting.java
@@ -1,3 +1,15 @@
+/*Input Format
+
+Every line of input will contain a String followed by an integer. 
+Each String will have a maximum of  alphabetic characters, and each integer will be in the inclusive range from  to .
+
+Output Format
+
+In each line of output there should be two columns: 
+The first column contains the String and is left justified using exactly  characters. 
+The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+*/
+
 package introduction.challanges;
 import java.util.Scanner;
 
