12a32a85e94f547b3e5f10004b702121c720d1a3
aishwaryarg1991 <30918908+aishwaryarg1991@users.noreply.github.com>
2018-05-15 17:42:25 -0400

    Code Commit

---
+++ src/introduction/challanges/JavaIntToString.java
@@ -0,0 +1,23 @@
+/*
+ * You are given an integer , you have to convert it into a string.
+ */
+package introduction.challanges;
+
+import java.util.Scanner;
+
+public class JavaIntToString {
+
+	public static void main(String[] args) {
+
+		
+		   Scanner in = new Scanner(System.in);
+		   int n = in .nextInt();
+		   in.close();
+		   //String s=???; Complete this line below
+
+	       String s = Integer.toString(n);
+
+	}
+
+
+}
