6b14f2ac1242a3328f1002ab99593bf2ee98651d
aishwaryarg1991 <30918908+aishwaryarg1991@users.noreply.github.com>
2018-05-18 22:09:07 -0400

    Comments added

--- src/introduction/challanges/TagContentExtractor.java
+++ src/introduction/challanges/TagContentExtractor.java
@@ -1,3 +1,16 @@
+/*
+Input Format
+The first line of input contains a single integer,  (the number of lines). 
+The  subsequent lines each contain a line of text.
+
+Output Format
+For each line, print the content enclosed within valid tags. 
+If a line contains multiple instances of valid content, print out each instance of valid content on a new line; if no valid content is found, print None.
+
+1)The name of the start and end tags must be same. The HTML code <h1>Hello World</h2> is not valid, because the text starts with an h1 tag and ends with a non-matching h2 tag.
+2)Tags can be nested, but content between nested tags is considered not valid. For example, in <h1><a>contents</a>invalid</h1>, contents is valid but invalid is not valid.
+3)Tags can consist of any printable characters.
+ */
 package introduction.challanges;
 
 import java.util.Scanner;
