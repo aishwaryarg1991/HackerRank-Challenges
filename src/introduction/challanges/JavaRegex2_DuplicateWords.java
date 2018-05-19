92868c42df7d107a1900be79f1f80993de6f725c
aishwaryarg1991 <30918908+aishwaryarg1991@users.noreply.github.com>
2018-05-18 22:03:58 -0400

    Code Commit

--- src/introduction/challanges/JavaRegex2_DuplicateWords.java
+++ src/introduction/challanges/JavaRegex2_DuplicateWords.java
@@ -16,27 +16,35 @@ public class JavaRegex2_DuplicateWords {
 
 	public static void main(String[] args) {
 
-        String regex = "\\b(\\w+)(\\s+\\1\\b)+";   //"/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); //correct Pattern flag
+		String regex = "\\b(\\w+)(\\s+\\1\\b)+"; // "/* Write a RegEx matching repeated words here. */";
+		Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); // correct Pattern flag, if you want to match words in case insensitive way 
 
-        Scanner in = new Scanner(System.in);
-        int numSentences = Integer.parseInt(in.nextLine());
-        
-        while (numSentences-- > 0) {
-            String input = in.nextLine();
-            
-            Matcher m = p.matcher(input);
-            
-            // Check for subsequences of input that match the compiled pattern
-            while (m.find()) {
-                input = input.replaceAll(m.group(),m.group(1) ); //(The regex to replace ,  The replacement)
-            }
-            
-            // Prints the modified sentence.
-            System.out.println(input);
-        }
-        
-        in.close();
-    }
+		Scanner in = new Scanner(System.in);
+		int numSentences = Integer.parseInt(in.nextLine());
 
+		while (numSentences-- > 0) {
+			String input = in.nextLine();
+
+			Matcher m = p.matcher(input);
+
+			// Check for subsequences of input that match the compiled pattern
+			while (m.find()) {
+				input = input.replaceAll(m.group(), m.group(1)); // (The regex to replace , The replacement)
+			}
+
+			// Prints the modified sentence.
+			System.out.println(input);
+		}
+
+		in.close();
+	}
+	/*
+	 
+	 * \b: look for word boundary (match only beginning of word instead of somewhere in the middle); 
+	 * (\w+): match one ore more word characters and remember them as a group(the()) to which later we can refer to using a number; so this matches a complete word and remembers it; 
+	 * \s+: match one or more space characters;
+	 * \1: match the word remembered in step 2; 
+	 * \b: like in step 1 – make sure it’s not a part of some longer word;
+	 *  (\s+\1\b)+: match one or more occurrences of the word captured in step 2.
+	 */
 }
